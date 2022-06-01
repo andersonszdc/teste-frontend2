c5766e464761049129cecddcd581c01f66c11494
Anderson Souza <andersonszdc@gmail.com>
2022-06-01 18:15:01 -0300

    set states to add employee data in list

--- src/components/customInput/index.tsx
+++ src/components/customInput/index.tsx
@@ -4,13 +4,47 @@ import { StyledCustomInput } from "./styles";
 type CustomInputProps = {
   label: string;
   placeholder: string;
+  value: string;
+  name: string;
+  setDataUser: React.Dispatch<
+    React.SetStateAction<{
+      name: string;
+      CPF: string;
+      RG: string;
+      setor: string;
+      cargo: string;
+      address: string;
+      birthDate: string;
+      telephone: string;
+      email: string;
+      status: boolean;
+    }>
+  >;
 };
 
-const CustomInput = ({ label, placeholder }: CustomInputProps) => {
+const CustomInput = ({
+  label,
+  placeholder,
+  value,
+  name,
+  setDataUser,
+}: CustomInputProps) => {
+  const handleChange = (e: any) => {
+    setDataUser((employee) => ({
+      ...employee,
+      [e.target.name]: e.target.value,
+    }));
+  };
   return (
     <StyledCustomInput>
       <label>{label}</label>
-      <input type="text" placeholder={placeholder} />
+      <input
+        type="text"
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+      />
     </StyledCustomInput>
   );
 };
--- src/components/registrationModal/index.tsx
+++ src/components/registrationModal/index.tsx
@@ -1,40 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomInput from "../customInput";
 import ToggleSwitch from "../toggleSwitch";
 import { StyledRegistrationModal, WrapperRegistrationModal } from "./styles";
 
 const RegistrationModal = ({ setPortalIsOpen, employees, setEmployees }) => {
+  const [dataUser, setDataUser] = useState({
+    name: "",
+    CPF: "",
+    RG: "",
+    setor: "",
+    cargo: "",
+    address: "",
+    birthDate: "",
+    telephone: "",
+    email: "",
+    status: false,
+  });
+
   const addEmployee = () => {
-    setEmployees((employees) => [
-      ...employees,
-      {
-        name: "aaaa",
-        CPF: "aaa",
-        RG: "aaa",
-        setor: "aaa",
-        cargo: "aaa",
-        address: "aaa",
-        birthDate: "aa",
-        telephone: "aaa",
-        email: "aaa",
-        status: false,
-      },
-    ]);
+    setEmployees((employees) => [...employees, dataUser]);
   };
+
+  useEffect(() => {
+    console.log(dataUser);
+  });
+
   return (
     <WrapperRegistrationModal>
       <StyledRegistrationModal>
         <h1 className="title">Cadastro de funcionário</h1>
         <div className="inputs">
-          <CustomInput label="Nome" placeholder="Kai Castro Lima" />
-          <CustomInput label="Data de nascimento" placeholder="01/01/2000" />
-          <CustomInput label="Telefone" placeholder="(21) 98765-4321" />
-          <CustomInput label="Email" placeholder="kai@mail.com" />
-          <CustomInput label="CPF" placeholder="123.456.789-00" />
-          <CustomInput label="RG" placeholder="12.345.678-9" />
-          <CustomInput label="Setor" placeholder="Sustentação" />
-          <CustomInput label="Cargo" placeholder="Analista de sistemas" />
-          <CustomInput label="Endereço" placeholder="Av. Lúcio Costa" />
+          <CustomInput
+            label="Nome"
+            placeholder="Kai Castro Lima"
+            value={dataUser.name}
+            name="name"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="Data de nascimento"
+            placeholder="01/01/2000"
+            value={dataUser.birthDate}
+            name="birthDate"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="Telefone"
+            placeholder="(21) 98765-4321"
+            value={dataUser.telephone}
+            name="telephone"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="Email"
+            placeholder="kai@mail.com"
+            value={dataUser.email}
+            name="email"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="CPF"
+            placeholder="123.456.789-00"
+            value={dataUser.CPF}
+            name="CPF"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="RG"
+            placeholder="12.345.678-9"
+            value={dataUser.RG}
+            name="RG"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="Setor"
+            placeholder="Sustentação"
+            value={dataUser.setor}
+            name="setor"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="Cargo"
+            placeholder="Analista de sistemas"
+            value={dataUser.cargo}
+            name="cargo"
+            setDataUser={setDataUser}
+          />
+          <CustomInput
+            label="Endereço"
+            placeholder="Av. Lúcio Costa"
+            value={dataUser.address}
+            name="address"
+            setDataUser={setDataUser}
+          />
           <ToggleSwitch />
         </div>
         <div className="action">
